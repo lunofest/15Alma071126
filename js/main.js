25b93a488940ea1629dd8d0005a02e6d8473ebe4
event <145824563+lunofest@users.noreply.github.com>
2026-09-15 21:02:47 -0300

    init commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// Registrar el Service Worker
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('js/service-worker.js')
+        .then(function(registration) {
+            console.log('Service Worker registrado con éxito:', registration);
+        })
+        .catch(function(error) {
+            console.log('Error al registrar el Service Worker:', error);
+        });
+}
